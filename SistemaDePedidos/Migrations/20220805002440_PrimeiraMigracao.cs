3287507469e2be1d23f28c2df73d45caf9617470
thiago <thiagoe99@hotmail.com>
2022-08-04 21:35:42 -0300

    Primeira migracao criada

---
+++ SistemaDePedidos/Migrations/20220805002440_PrimeiraMigracao.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+//este arquivo contem todas alteracoes que irao ocorrer na base de dados
+namespace SistemaDePedidosEFCoreConsole.Migrations
+{
+    public partial class PrimeiraMigracao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //criar funcao na base de dados dentro da funcao up, assim ela é executada junto a migracao
+            //migrationBuilder.Sql("CREATE FUNCTION ...");
+            
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "VARCHAR(80)", nullable: false),
+                    Phone = table.Column<string>(type: "CHAR(11)", nullable: false),
+                    CEP = table.Column<string>(type: "CHAR(8)", nullable: false),
+                    Estado = table.Column<string>(type: "CHAR(2)", nullable: false),
+                    Cidade = table.Column<string>(maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodigoBarras = table.Column<string>(type: "VARCHAR(14)", nullable: false),
+                    Descricao = table.Column<string>(type: "VARCHAR(60)", nullable: false),
+                    Valor = table.Column<decimal>(nullable: false),
+                    TipoProduto = table.Column<string>(nullable: false),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClienteID = table.Column<int>(nullable: false),
+                    IniciadoEm = table.Column<DateTime>(nullable: false, defaultValueSql: "GETDATE()"),
+                    FinalizadoEm = table.Column<DateTime>(nullable: false),
+                    TipoFrete = table.Column<string>(nullable: false),
+                    Status = table.Column<string>(nullable: false),
+                    Observacao = table.Column<string>(type: "VARCHAR(512)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pedido_Clientes_ClienteID",
+                        column: x => x.ClienteID,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PedidoItens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PedidoID = table.Column<int>(nullable: false),
+                    ProdutoID = table.Column<int>(nullable: false),
+                    Quantidade = table.Column<int>(nullable: false, defaultValue: 1),
+                    Valor = table.Column<decimal>(nullable: false),
+                    Desconto = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PedidoItens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PedidoItens_Pedido_PedidoID",
+                        column: x => x.PedidoID,
+                        principalTable: "Pedido",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PedidoItens_Produtos_ProdutoID",
+                        column: x => x.ProdutoID,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "idx_cliente_telefone",
+                table: "Clientes",
+                column: "Phone");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedido_ClienteID",
+                table: "Pedido",
+                column: "ClienteID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoItens_PedidoID",
+                table: "PedidoItens",
+                column: "PedidoID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoItens_ProdutoID",
+                table: "PedidoItens",
+                column: "ProdutoID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //remover a funcao criada no up
+            //migrationBuilder.Sql("DROP FUNCTION ...");
+
+            migrationBuilder.DropTable(
+                name: "PedidoItens");
+
+            migrationBuilder.DropTable(
+                name: "Pedido");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
